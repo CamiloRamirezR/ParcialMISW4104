47a1c660acfd7a08bca76e065e7dfbdff6576d99
Camilo Ramírez R <c.ramirezr2@uniandes.edu.co>
2023-04-29 17:25:08 -0500

    Se crea servicio para obtener datos del back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -13,7 +14,8 @@ import { PlantaListComponent } from './planta/planta-list/planta-list.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PlantaModule
+    PlantaModule,
+    HttpClientModule
   ],
   exports: [PlantaListComponent],
   providers: [],
